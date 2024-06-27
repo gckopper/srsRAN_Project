f1d23581f586afec58b761fb8446145bd611b4ed
gckopper <gabriel@gck.dev.br>
2024-06-27 17:50:24 -0300

    Allows for slice-level prb quota control action to act in a slice level

--- lib/du_manager/procedures/du_ue_ric_configuration_procedure.cpp
+++ lib/du_manager/procedures/du_ue_ric_configuration_procedure.cpp
@@ -21,7 +21,11 @@
  */
 
 #include "du_ue_ric_configuration_procedure.h"
+#include "srsran/ran/resource_block.h"
+#include "srsran/ran/s_nssai.h"
 #include "srsran/support/async/execute_on.h"
+#include <cassert>
+#include <vector>
 
 using namespace srsran;
 using namespace srs_du;
@@ -29,7 +33,7 @@ using namespace srs_du;
 du_ue_ric_configuration_procedure::du_ue_ric_configuration_procedure(const du_mac_sched_control_config& request_,
                                                                      du_ue_manager_repository&          ue_mng_,
                                                                      const du_manager_params&           du_params_) :
-  request(request_), ue_mng(ue_mng_), du_params(du_params_)
+  logger(srslog::fetch_basic_logger("DU-RIC")), request(request_), ue_mng(ue_mng_), du_params(du_params_)
 {
 }
 
@@ -50,37 +54,64 @@ void du_ue_ric_configuration_procedure::operator()(coro_context<async_task<du_ma
 
 manual_event<du_mac_sched_control_config_response>& du_ue_ric_configuration_procedure::dispatch_ue_config_task()
 {
-  // Find UE context based on F1AP UE ID.
-  ue = ue_mng.find_f1ap_ue_id(static_cast<gnb_du_ue_f1ap_id_t>(request.ue_id));
-  if (ue == nullptr) {
-    du_mac_sched_control_config_response fail{false, false, false};
+  // Start as failed and change it if needed
+  du_mac_sched_control_config_response fail{false, false, false};
+  // TODO go through all parameters
+  s_nssai_t  requested_nssai = request.param_list[0].rrm_policy_group->pol_member.s_nssai;
+  const auto filter          = [requested_nssai](const du_ue* test_ue) {
+    if (test_ue->bearers.drbs().empty()) {
+      return false;
+    }
+    s_nssai_t ue_nssai = test_ue->bearers.drbs().begin()->second->s_nssai;
+    if (ue_nssai.sst != requested_nssai.sst) {
+      return false;
+    }
+    if (!(ue_nssai.sd.has_value() && requested_nssai.sd.has_value())) {
+      return false;
+    }
+    if (ue_nssai.sd.value() != requested_nssai.sd.value()) {
+      return false;
+    }
+    return true;
+  };
+  std::vector<du_ue*> ues = ue_mng.find_ues(filter);
+  if (ues.empty()) {
     ue_config_completed.set(fail);
     return ue_config_completed;
   }
+  logger.debug(
+      "{} UEs were found for SST {} and SD {}", ues.size(), requested_nssai.sst, requested_nssai.sd.value_or(0));
 
-  // Dispatch UE configuration to UE task loop inside the UE manager.
-  ue_mng.schedule_async_task(
-      ue->ue_index, launch_async([this](coro_context<async_task<void>>& ctx) {
-        CORO_BEGIN(ctx);
+  for (du_ue* test_ue : ues) {
+    // Dispatch UE configuration to UE task loop inside the UE manager.
+    ue_mng.schedule_async_task(test_ue->ue_index, launch_async([this, test_ue](coro_context<async_task<void>>& ctx) {
+                                 CORO_BEGIN(ctx);
 
-        // Await for UE configuration completion.
-        CORO_AWAIT_VALUE(const mac_ue_reconfiguration_response result, handle_mac_config());
+                                 // Await for UE configuration completion.
+                                 // TODO Find a way to efficiently merge all results
+                                 // CORO_AWAIT_VALUE(const mac_ue_reconfiguration_response result,
+                                 // handle_mac_config(test_ue));
+                                 CORO_AWAIT(handle_mac_config(test_ue));
 
-        // Signal completion of UE configuration to external coroutine.
-        ue_config_completed.set(du_mac_sched_control_config_response{result.result, result.result, result.result});
+                                 // TODO Signal completion of UE configuration to external coroutine.
 
-        CORO_RETURN();
-      }));
+                                 CORO_RETURN();
+                               }));
+  }
+
+  // TODO always returns true
+  ue_config_completed.set(du_mac_sched_control_config_response({true, true, true}));
+  logger.info("ue reconfiguration completed with {}", true);
 
   return ue_config_completed;
 }
 
-async_task<mac_ue_reconfiguration_response> du_ue_ric_configuration_procedure::handle_mac_config()
+async_task<mac_ue_reconfiguration_response> du_ue_ric_configuration_procedure::handle_mac_config(du_ue* test_ue)
 {
   mac_ue_reconfiguration_request mac_request;
 
-  mac_request.ue_index    = ue->ue_index;
-  mac_request.crnti       = ue->rnti;
+  mac_request.ue_index    = test_ue->ue_index;
+  mac_request.crnti       = test_ue->rnti;
   mac_request.pcell_index = to_du_cell_index(0);
 
   // Configure UE resource allocation parameters.
@@ -105,6 +136,10 @@ async_task<mac_ue_reconfiguration_response> du_ue_ric_configuration_procedure::h
                                            ? req.num_harq_retransmissions.value()
                                            : du_params.mac.sched_cfg.ue.max_nof_harq_retxs;
   res_alloc_cfg.max_pusch_harq_retxs = res_alloc_cfg.max_pdsch_harq_retxs;
+  logger.info("Setting the PRBs for {} as [{}, {}]",
+              mac_request.crnti,
+              res_alloc_cfg.pdsch_grant_size_limits.start(),
+              res_alloc_cfg.pdsch_grant_size_limits.stop());
 
   return du_params.mac.ue_cfg.handle_ue_reconfiguration_request(mac_request);
 }
